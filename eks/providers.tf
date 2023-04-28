bd5e69bb1c86c0ff81d287cadc1099074ba067ee
Caio Queiroz <ccesar.qp@gmail.com>
2023-04-28 13:38:04 -0300

    commit files so far WIP

--- eks/providers.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-deploy-primo"
-    key    = "primoti-eks"
-    region = "us-east-2"
-  }
-}
-
--- rancher.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-curl https://releases.rancher.com/install-docker/19.03.sh | sh
-docker run -d --restart=unless-stopped \
-  -p 80:80 -p 443:443 \
-  --privileged \
-  rancher/rancher:latest
