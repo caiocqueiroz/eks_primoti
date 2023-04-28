324c4f49aab45a747cdad8f2612e15b68b427295
Caio Queiroz <ccesar.qp@gmail.com>
2023-04-28 12:54:56 -0300

    fix versions.tf

--- eks/versions.tf
+++ eks/versions.tf
@@ -2,22 +2,22 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.20.0"
+      version = ">= 3.63.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "3.0.0"
+      version = "3.1.0"
     }
 
     local = {
       source  = "hashicorp/local"
-      version = "2.0.0"
+      version = "2.1.0"
     }
 
     null = {
       source  = "hashicorp/null"
-      version = "3.0.0"
+      version = "3.1.0"
     }
 
     template = {
@@ -27,10 +27,10 @@ terraform {
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.0.1"
+      version = ">= 2.6.1"
     }
   }
 
-  required_version = "~> 0.14"
+  required_version = "~> 1.0.0"
 }
 
