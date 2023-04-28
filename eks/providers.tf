bd7ecc256ffeb1079b72b71757dd71814d362d14
Caio Queiroz <ccesar.qp@gmail.com>
2023-04-28 12:50:54 -0300

    fix backend tf

--- eks/providers.tf
+++ eks/providers.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-deploy-jonjon"
-    key    = "devops-ninja-eks"
+    bucket = "terraform-deploy-primo"
+    key    = "primoti-eks"
     region = "us-east-2"
   }
 }
