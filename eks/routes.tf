fc079240dd384fb1aa9fa9eab396f4f81f5c2796
Caio Queiroz <ccesar.qp@gmail.com>
2023-04-28 15:11:01 -0300

    fmt

--- eks/routes.tf
+++ eks/routes.tf
@@ -3,8 +3,8 @@ resource "aws_route_table" "private" {
 
   route = [
     {
-      cidr_block                 = "0.0.0.0/0"
-      nat_gateway_id             = aws_nat_gateway.nat.id
+      cidr_block     = "0.0.0.0/0"
+      nat_gateway_id = aws_nat_gateway.nat.id
       # carrier_gateway_id         = ""
       # destination_prefix_list_id = ""
       # egress_only_gateway_id     = ""
@@ -29,8 +29,8 @@ resource "aws_route_table" "public" {
 
   route = [
     {
-      cidr_block                 = "0.0.0.0/0"
-      gateway_id                 = aws_internet_gateway.igw.id
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.igw.id
       # nat_gateway_id             = ""
       # carrier_gateway_id         = ""
       # destination_prefix_list_id = ""
