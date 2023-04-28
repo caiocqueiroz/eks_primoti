f5ef4a0fe221aa17ad3934eec7474b654a76a546
Caio Queiroz <ccesar.qp@gmail.com>
2023-04-28 15:28:43 -0300

    fix route tables

--- eks/routes.tf
+++ eks/routes.tf
@@ -1,11 +1,9 @@
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
-
 }
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
-
 }
 
 resource "aws_route" "private_route" {
@@ -13,21 +11,12 @@ resource "aws_route" "private_route" {
   route_table_id         = aws_route_table.private
   nat_gateway_id         = aws_nat_gateway.nat.id
   destination_cidr_block = "0.0.0.0/0"
-
-  tags = {
-    Name = "private-eks"
-  }
 }
 
 resource "aws_route" "public_route" {
 
   route_table_id = aws_route_table.public
   gateway_id     = aws_internet_gateway.igw.id
-
-
-  tags = {
-    Name = "public-eks"
-  }
 }
 
 resource "aws_route_table_association" "private-us-east-1a" {
