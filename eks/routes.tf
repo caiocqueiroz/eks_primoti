57f38f7abb1ccee16cd0ee5422f6ad0846d88057
Caio Queiroz <ccesar.qp@gmail.com>
2023-04-28 15:17:22 -0300

    fix

--- eks/routes.tf
+++ eks/routes.tf
@@ -5,17 +5,6 @@ resource "aws_route_table" "private" {
     {
       cidr_block                 = "0.0.0.0/0"
       nat_gateway_id             = aws_nat_gateway.nat.id
-      carrier_gateway_id         = ""
-      destination_prefix_list_id = ""
-      egress_only_gateway_id     = ""
-      gateway_id                 = ""
-      instance_id                = ""
-      ipv6_cidr_block            = ""
-      local_gateway_id           = ""
-      network_interface_id       = ""
-      transit_gateway_id         = ""
-      vpc_endpoint_id            = ""
-      vpc_peering_connection_id  = ""
     },
   ]
 
@@ -31,17 +20,6 @@ resource "aws_route_table" "public" {
     {
       cidr_block                 = "0.0.0.0/0"
       gateway_id                 = aws_internet_gateway.igw.id
-      nat_gateway_id             = ""
-      carrier_gateway_id         = ""
-      destination_prefix_list_id = ""
-      egress_only_gateway_id     = ""
-      instance_id                = ""
-      ipv6_cidr_block            = ""
-      local_gateway_id           = ""
-      network_interface_id       = ""
-      transit_gateway_id         = ""
-      vpc_endpoint_id            = ""
-      vpc_peering_connection_id  = ""
     },
   ]
 
